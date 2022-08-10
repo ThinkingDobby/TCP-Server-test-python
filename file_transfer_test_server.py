0dda2825b7d990db59383df4ce38219cf506fef0
ThinkingDobby <chiho1221@naver.com>
2022-08-10 16:39:37 +0900

    기본 프로토콜 구현 중 임시 저장

--- file_transfer_test_server.py
+++ file_transfer_test_server.py
@@ -1,6 +1,7 @@
 import os
 
 from basic_test_server import BasicTestServer
+from protocols.basic_protocol import BasicProtocol
 
 
 class FileTransferTestServer(BasicTestServer):
@@ -9,18 +10,18 @@ class FileTransferTestServer(BasicTestServer):
         self.clntSock, addr = self.servSock.accept()
         print("Client Address: ", addr)
 
-        size = self.clntSock.recv(BasicTestServer.bufSize)
-        size = int(size)
         cwd = os.getcwd()
+        pt = BasicProtocol()
         with open(cwd + '/' + 'temp.wav', 'wb') as f:
+            data = self.clntSock.recv(BasicTestServer.bufSize)
+            data = data[5:]
+
             while True:
+                f.write(data)
                 data = self.clntSock.recv(BasicTestServer.bufSize)
                 if not data:
                     break
-                else:
-                    f.write(data)
 
-        print('size: %d' % size)
         self.clntSock.sendall("Transfer Finished".encode())
 
         self.stopServer()
