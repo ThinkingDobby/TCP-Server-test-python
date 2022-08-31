249514e45aea09f5c9a3ce4567559561be29bf51
ThinkingDobby <chiho1221@naver.com>
2022-08-31 18:15:04 +0900

    실시간 전송 대응 추가
    <FIN>
    실시간 전송 대응 type3으로 구현
    종료문자는 임시로 ';;'로 설정
    .pcm포맷으로 수신

--- protocols/basic_wav_receiving_protocol.py
+++ protocols/basic_wav_receiving_protocol.py
@@ -16,15 +16,16 @@ class BasicWAVReceivingProtocol:
     def save(self, header):
         # 타입 확인
         self._typ = header[0]
-        # 파일 크기 계산
-        self._fileSize = self.getSize(header[1:5])
-        # print(self._typ, self._fileSize)
+        print(self._typ, header)
 
         # 임시 타입
         if self._typ == -1:
             print("temp type")
         # 기본 수신 - 파일로 저장
         elif self._typ == 1:
+            # 파일 크기 계산
+            self._fileSize = self.getSize(header[1:5])
+
             cwd = os.getcwd()
             f = open(cwd + '/' + 'temp.wav', 'wb')   # 파일명 - temp.wav 고정 (임시)
             nowSize = 0
@@ -40,12 +41,35 @@ class BasicWAVReceivingProtocol:
             f.close()
         # 메모리에만 저장
         elif self._typ == 2:
+            # 파일 크기 계산
+            self._fileSize = self.getSize(header[1:5])
+
             nowSize = 0
             while True:
                 data = self._clntSock.recv(self._bufSize)
                 nowSize += len(data)
                 if nowSize >= self._fileSize:
                     break
+        elif self._typ == 3:
+            cwd = os.getcwd()
+            f = open(cwd + '/' + 'temp.pcm', 'wb')
+            nowSize = 0
+            # 저장
+            while True:
+                data = self._clntSock.recv(1024)
+                print(data, end='')
+                if data == b';;':
+                    print()
+                    print("FIN_CODE Received")
+                    break
+                nowSize += len(data)
+                f.write(data)
+
+            print(nowSize)
+
+            f.flush()
+            os.fsync(f)
+            f.close()
 
     # 4바이트 연결해 파일 크기 계산 후 반환
     def getSize(self, data):
