a15e1fe8888b6bf183242b44b258c9e9f84ef524
ThinkingDobby <chiho1221@naver.com>
2022-09-11 15:27:28 +0900

    프로토콜 v1.1 적용 테스트

--- protocols/basic_wav_receiving_protocol.py
+++ protocols/basic_wav_receiving_protocol.py
@@ -4,27 +4,33 @@ import os
 class BasicWAVReceivingProtocol:
     def __init__(self, clntSock, bufSize):
         self._typ = -1  # 전송 타입
-        self._fileSize = 0  # 파일 전체 크기
         self._bufSize = bufSize
         self._clntSock = clntSock
         
     def getHeader(self):
         # 헤더 수신
-        header = self._clntSock.recv(5)
+        header = self._clntSock.recv(2)
         return header
 
     def save(self, header):
-        # 타입 확인
-        self._typ = header[0]
-        # 파일 크기 계산
-        self._fileSize = self.getSize(header[1:5])
-        # print(self._typ, self._fileSize)
+        # 시작코드 확인
+        if header[0:1] != b'[':
+            print("Start Code Error")
 
-        # 임시 타입
-        if self._typ == -1:
-            print("temp type")
-        # 기본 수신 - 파일로 저장
-        elif self._typ == 1:
+        # 타입 확인
+        self._typ = header[1]
+
+        # wav파일 수신
+        if self._typ == 1:
+            # 헤더 모두 수신 (총 10바이트)
+            header += self._clntSock.recv(8)
+            # 메시지 크기
+            msgSize = header[2]
+            # 확장자
+            ext = header[3:6]
+            # 파일 크기
+            fileSize = self.getSize(header[6:10])
+
             cwd = os.getcwd()
             f = open(cwd + '/' + 'temp.wav', 'wb')   # 파일명 - temp.wav 고정 (임시)
             nowSize = 0
@@ -32,20 +38,22 @@ class BasicWAVReceivingProtocol:
             while True:
                 data = self._clntSock.recv(self._bufSize)
                 nowSize += len(data)
-                f.write(data)
-                if nowSize >= self._fileSize:
+
+                if nowSize >= fileSize:
+                    f.write(data[:fileSize - nowSize])
+                    # 종료코드 확인
+                    if data[fileSize - nowSize:] != b']':
+                        print("End Code Error")
                     break
+
+                f.write(data)
+
             f.flush()
             os.fsync(f)
             f.close()
-        # 메모리에만 저장
+        # pcm파일 실시간 수신
         elif self._typ == 2:
-            nowSize = 0
-            while True:
-                data = self._clntSock.recv(self._bufSize)
-                nowSize += len(data)
-                if nowSize >= self._fileSize:
-                    break
+            pass
 
     # 4바이트 연결해 파일 크기 계산 후 반환
     def getSize(self, data):
